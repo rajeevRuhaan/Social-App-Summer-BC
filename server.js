2ba2f37f1edc5e47cb29232527f27ce809185ff6
AlvinBach <quocanh1990bach@gmail.com>
2021-05-24 21:26:16 +0300

    fixing dotenv

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -17,6 +18,16 @@ app.use('/api/users', require('./routes/api/userRoutes'));
 app.use('/api/auth', require('./routes/api/authRoutes'));
 app.use('/api/profile', require('./routes/api/profileRoutes'));
 app.use('/api/posts', require('./routes/api/postRoutes'));
+
+//production mode vs development mode
+if (process.env.NODE_ENV === 'production') {
+  //serve up client/build
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 //Start the server
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
