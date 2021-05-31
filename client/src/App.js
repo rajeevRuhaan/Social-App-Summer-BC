6d4df4fc31f431c9fe07bda8248880318eb2784c
Khan Fayjul <khanuxd@gmail.com>
2021-05-31 10:57:02 +0300

    file structure change and responsive nav finalized

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Foooter/Footer';
+
 import './App.scss';
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <Router>
+      <Header />
+      <Main />
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/Main/HeroBanner/HeroBanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroBanner = () => {
+    return (
+        <div>
+            Hero Banner section
+        </div>
+    );
+};
+
+export default HeroBanner;
