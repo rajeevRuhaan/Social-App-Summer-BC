d543ab474780cdc6988ea681661922aadc5c2c26
AlvinBach <quocanh1990bach@gmail.com>
2021-06-06 23:08:37 +0300

    fixing navbar profile

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,13 +3,14 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { logout } from '../../redux/actions/auth';
 
 const CustomNavbar = () => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const auth = useSelector((state) => state.auth);
 
   return (
     <Navbar
@@ -29,7 +30,7 @@ const CustomNavbar = () => {
             <IndexLinkContainer to='/'>
               <Nav.Link>Home</Nav.Link>
             </IndexLinkContainer>
-            <LinkContainer to='/profile'>
+            <LinkContainer to={`/profile/${auth.user._id}`}>
               <Nav.Link>Profile</Nav.Link>
             </LinkContainer>
             <Nav.Link
