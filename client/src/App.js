9ae98a010612e2a59686dfbc937628f48c241de5
Khan Fayjul <khanuxd@gmail.com>
2021-06-07 15:54:27 +0300

    pages folder path fixed on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,13 @@ import Container from 'react-bootstrap/Container';
 
 import './App.scss';
 import store from './redux/store';
-<<<<<<< HEAD
-import SignInPage from './pages/SignInPage/SignInPage';
-=======
 import Footer from './components/Footer/Footer';
-import SignInPage from './Pages/SignInPage/SignInPage';
->>>>>>> 4f99c40a4e54d3e467ffec4f620565e7169c5470
+import SignInPage from './pages/SignInPage/SignInPage';
 import { loadUser } from './redux/actions/auth';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-<<<<<<< HEAD
 import DashboardPage from './pages/DashboardPage/DashboardPage';
 import HomePage from './pages/HomePage/HomePage';
-import ProfilePage from './pages/ProfilePage/ProfilePage';
-=======
-import DashboardPage from './Pages/DashboardPage/DashboardPage';
-import HomePage from './Pages/HomePage/HomePage';
->>>>>>> 4f99c40a4e54d3e467ffec4f620565e7169c5470
 
 const App = () => {
   useEffect(() => {
