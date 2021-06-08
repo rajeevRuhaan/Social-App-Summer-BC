f6aecde0bcff00477f67fed5d6d0b17c4120e82d
AlvinBach <quocanh1990bach@gmail.com>
2021-06-08 09:50:39 +0300

    about me

--- client/src/components/AboutMe/AboutMe.js
+++ client/src/components/AboutMe/AboutMe.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import ProgressBar from 'react-bootstrap/ProgressBar';
+import Moment from 'react-moment';
 
 const AboutMe = ({
   social,
@@ -19,10 +19,22 @@ const AboutMe = ({
   user,
   experience,
   education,
+  date,
 }) => {
+  const auth = useSelector((state) => state.auth);
+
   return (
     <Card className='shadow-sm mt-3 p-3'>
-      <h6>About Me</h6>
+      {auth.user._id === user._id ? (
+        <Link to={`/settings/${user._id}`}>
+          <div className='d-flex justify-content-between'>
+            <h6>About Me</h6>
+            <i className='fas fa-edit'></i>
+          </div>
+        </Link>
+      ) : (
+        <h6>About Me</h6>
+      )}
       <Row className='mt-3'>
         <Col>
           <div className='border-top w-100'></div>
@@ -40,7 +52,7 @@ const AboutMe = ({
       </Row>
       <Row className='mt-3'>
         <Col>
-          <i class='fas fa-map-marker-alt'></i>{' '}
+          <i className='fas fa-map-marker-alt'></i>{' '}
           <small>{location ? location : 'Your Location'}</small>
         </Col>
       </Row>
@@ -55,6 +67,28 @@ const AboutMe = ({
           <i className='fab fa-github'></i>{' '}
           <small>{githubusername ? githubusername : 'Your Github'}</small>
         </Col>
+      </Row>
+      <Row className='mt-3'>
+        <Col>
+          <i className='fas fa-calendar-day'></i>{' '}
+          <small>
+            Joined <Moment format='MMMM, YYYY'>{date}</Moment>
+          </small>
+        </Col>
+      </Row>
+      <Row className='mt-3'>
+        <Col>
+          <div className='border-top w-100'></div>
+        </Col>
+      </Row>
+      <Row className='mt-3'>
+        {skills.length === 0
+          ? 'Enter Your Skills'
+          : skills.map((skill, i) => (
+              <Col key={i}>
+                <i className='far fa-check-square'>{skill}</i>
+              </Col>
+            ))}
       </Row>
     </Card>
   );
